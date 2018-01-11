c13bc1d7959d5a6249e0d74f32bba72d9a6e45d6
prapull <pmavardhan@gmail.com>
2018-01-11 15:08:40 -0600

    human readable format for date

--- src/main/java/com/sample/project/repository/impl/ApplicationRepository.java
+++ src/main/java/com/sample/project/repository/impl/ApplicationRepository.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Repository;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -31,7 +34,7 @@ public class ApplicationRepository implements IApplicationRepository {
                 try {
                     List<String> order = Arrays.asList(eachLine.split("\\s+"));
                     entityList.add(new Order(order.get(0), Long.parseLong(order.get(1))));
-                    writer.write(eachLine);
+                    writer.write(order.get(0) +  "   " + convertEpochToDate(order.get(1)));
                     writer.newLine();
                 } catch (IOException e) {
                     logger.error("Exception while writing output to the destination file", e);
@@ -42,5 +45,11 @@ public class ApplicationRepository implements IApplicationRepository {
             logger.error("Write to the file failed:", e);
         }
         return entityList;
+    }
+
+    private String convertEpochToDate(String epoch) {
+        Date date = new Date(Long.parseLong(epoch));
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return format.format(date);
     }
 }

2bc579de2e10eee6b0590bc3e7c13790b4636813
prapull <pmavardhan@gmail.com>
2018-01-11 14:45:02 -0600

    remove unused imports

--- src/main/java/com/sample/project/Application.java
+++ src/main/java/com/sample/project/Application.java
@@ -1,11 +1,8 @@
 package com.sample.project;
-import com.sample.project.config.SpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(SpringConfig.class)
 public class Application {
 
     public static void main(String... args) throws Exception {
